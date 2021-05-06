5ea559bd04470ed4daba1646486516640131e45f
Miguel Nunez <71807189+iamcodefoxx@users.noreply.github.com>
2021-05-05 19:04:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementsByClassName("fab fa-youtube")[0].addEventListener("click", function(){
+        window.open("https://www.youtube.com/watch?v=MKgDQjZwI2o");
+});
+
+document.getElementsByClassName("fab fa-youtube")[1].addEventListener("click", function(){
+        window.open("https://www.youtube.com/watch?v=wtqbOFYivxw");
+});
+
+document.getElementsByClassName("fab fa-youtube")[2].addEventListener("click", function(){
+        window.open("https://www.youtube.com/watch?v=H5tuwAO-_Kg");
+});
+
+document.getElementsByClassName("article")[0].addEventListener("click", function(){
+        window.open("https://iamcodefoxx.github.io/Weather-App/");
+});
+
+document.getElementsByClassName("article")[1].addEventListener("click", function(){
+        window.open("https://iamcodefoxx.github.io/Quiz-App/");
+});
+
+document.getElementsByClassName("article")[2].addEventListener("click", function(){
+        window.open("https://iamcodefoxx.github.io/Marvel-API/");
+});
